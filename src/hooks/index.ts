942af8d3b90bec48081f76f268608f8188b9c6bb
NBoris94 <nboris1994@yandex.ru>
2023-01-19 11:35:57 +0300

    Добавил новые хуки

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,9 +1,17 @@
 import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
 import {AppDispatch, AppState} from "@/store/index"
 import { useFocusTrap } from './useFocusTrap'
+import { useDebounce } from './useDebounce'
+import { useSelectAll } from './useSelectAll'
 
 const useAppDispatch = () => useDispatch<AppDispatch>()
 
 const useAppSelector: TypedUseSelectorHook<AppState> = useSelector
 
-export { useAppDispatch, useAppSelector, useFocusTrap }
+export {
+  useAppDispatch,
+  useAppSelector,
+  useFocusTrap,
+  useDebounce,
+  useSelectAll
+}
